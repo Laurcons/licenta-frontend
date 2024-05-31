2578256cd3bddb8e73a7da2eb80da0e90b89d403
Laurentiu Pricop <laurcons@outlook.com>
2024-05-31 23:50:05 +0300

    fix: login

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -4,15 +4,17 @@ import NewTripFormSection from './NewTripFormSection';
 import PreviousTripsSection from './PreviousTripsSection';
 import ScanQRModal from './ScanQRModal';
 import LoginSection from './LoginSection';
+import { useAuth } from '../../lib/hooks/useAuth';
 
 export default function HomePage() {
   const [isScanningQr, setIsScanningQr] = useState(false);
+  const { user, isLoading: isUserLoading } = useAuth();
 
   return (
     <>
       <LiveLocationSection />
       <NewTripFormSection onScanClick={() => setIsScanningQr(true)} />
-      <LoginSection />
+      {!isUserLoading && !user && <LoginSection />}
       <PreviousTripsSection />
       <ScanQRModal
         isOpen={isScanningQr}
